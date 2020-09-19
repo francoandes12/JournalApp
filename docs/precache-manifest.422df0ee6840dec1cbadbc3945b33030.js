7e09df285a8524c741c5f4423591c982995f730b
francoandes12 <francoande12@gmail.com>
2020-09-19 20:44:09 -0300

    journal deploy 2

---
+++ docs/precache-manifest.422df0ee6840dec1cbadbc3945b33030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc85fc667f6c30d37329afde1a7e3e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b41182d41a244911312",
+    "url": "/static/css/main.bb3b1f61.chunk.css"
+  },
+  {
+    "revision": "efa00d048eec207319d8",
+    "url": "/static/js/2.a7f3db50.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.a7f3db50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b41182d41a244911312",
+    "url": "/static/js/main.743d6747.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
